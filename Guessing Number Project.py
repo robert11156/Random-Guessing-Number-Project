644be6b651f09e96fa008e9c04f902be10d58f69
robert11156 <robert11156@gmail.com>
2017-03-02 14:56:05 -0500

    Update Guessing Number Project.py

--- Guessing Number Project.py
+++ Guessing Number Project.py
@@ -2,8 +2,8 @@
 # the player know if the guess is too high, too low
 # or guess the number correctly
 import random
-total=0
 
+the_number = random.randint(1, 100)
 name = input("Enter your name: ")
 print("Hi," ,name, " I am thinking of a number between 1 and 100. Guess the number")
 
@@ -11,15 +11,12 @@ print("Hi," ,name, " I am thinking of a number between 1 and 100. Guess the numb
 print("Welcome to my Guessing Game.")
 
 
-# set the initial values
-the_number = random.randint(1, 100)
-guess = int(input("Take a guess: "))
-tries = 1
+tries = 0
 
 # guessing loop
 while tries <6:
-    guess = input("Guess the number: ")
-    tries = tries + 1 
+    guess = int(input("Guess the number: "))
+    tries = tries+1 
     if guess > the_number:
         print("The number is too large")
     elif guess < the_number:
